bd7e824ef216ade65ece1e595577357fbc72ea7d
Jack Carmichael <carmichaeljr@outlook.com>
2024-11-21 01:22:31 -0700

    fmt

--- src/csv/FromStructs.go
+++ src/csv/FromStructs.go
@@ -103,7 +103,7 @@ func FromStructs[R any](src iter.Iter[R], opts *options) iter.Iter[[]string] {
 }
 
 func getValAsString[R any](r *R, sIdx structIndex, opts *options) (string, error) {
-	fmtStr:="%v"
+	fmtStr := "%v"
 	v := stdReflect.ValueOf(r).Elem().Field(int(sIdx))
 	actualVal := v.Interface()
 
@@ -114,26 +114,36 @@ func getValAsString[R any](r *R, sIdx structIndex, opts *options) (string, error
 		return v.Interface().(time.Time).Format(opts.dateTimeFormat), nil
 	}
 
-	zeroVal:=stdReflect.Zero(v.Type())
+	zeroVal := stdReflect.Zero(v.Type())
 	switch v.Kind() {
 	case stdReflect.Bool:
-		fmtStr="%t"
-	case stdReflect.Uint: fallthrough
-	case stdReflect.Uint8: fallthrough
-	case stdReflect.Uint16: fallthrough
-	case stdReflect.Uint32: fallthrough
-	case stdReflect.Uint64: fallthrough
-	case stdReflect.Int: fallthrough
-	case stdReflect.Int8: fallthrough
-	case stdReflect.Int16: fallthrough
-	case stdReflect.Int32: fallthrough
+		fmtStr = "%t"
+	case stdReflect.Uint:
+		fallthrough
+	case stdReflect.Uint8:
+		fallthrough
+	case stdReflect.Uint16:
+		fallthrough
+	case stdReflect.Uint32:
+		fallthrough
+	case stdReflect.Uint64:
+		fallthrough
+	case stdReflect.Int:
+		fallthrough
+	case stdReflect.Int8:
+		fallthrough
+	case stdReflect.Int16:
+		fallthrough
+	case stdReflect.Int32:
+		fallthrough
 	case stdReflect.Int64:
-		fmtStr="%d"
-	case stdReflect.Float32: fallthrough
+		fmtStr = "%d"
+	case stdReflect.Float32:
+		fallthrough
 	case stdReflect.Float64:
-		fmtStr="%f"
+		fmtStr = "%f"
 	case stdReflect.String:
-		fmtStr="%s"
+		fmtStr = "%s"
 		if len(actualVal.(string)) == 0 {
 			break
 		}
